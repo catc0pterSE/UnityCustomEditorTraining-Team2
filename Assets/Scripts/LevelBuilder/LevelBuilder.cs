4f73e824437983a207fde45594706517d5545942
catc0pterSE <kasalapoff@gmail.com>
2022-12-10 16:26:27 +0300

    lvlBuilder - rotation

--- Assets/Scripts/LevelBuilder/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder/LevelBuilder.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 public class LevelBuilder : EditorWindow
 {
     private const string _pathBuildings = "Assets/Editor Resources/Buildings";
     private const string _pathProps = "Assets/Editor Resources/Props";
     private const string _pathPlants = "Assets/Editor Resources/Plants";
+    private const float _rotationSpeed = 2;
 
     private Vector2 _scrollPosition;
     private int _selectedElement;
@@ -34,7 +37,7 @@ public class LevelBuilder : EditorWindow
     private void OnGUI()
     {
         _selectedTabNumber = GUILayout.Toolbar(_selectedTabNumber, _tabNames);
-
+        // position.width - размеры окна
         switch (_selectedTabNumber) //TODO: scale grid size automatically
         {
             case 0:
@@ -59,9 +62,11 @@ public class LevelBuilder : EditorWindow
             EditorGUILayout.LabelField("Created Object Settings");
             Transform createdTransform = _createdObject.transform;
             createdTransform.position = EditorGUILayout.Vector3Field("Position", createdTransform.position);
-            createdTransform.rotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Position", createdTransform.rotation.eulerAngles));
-            createdTransform.localScale = EditorGUILayout.Vector3Field("Position", createdTransform.localScale);
+            createdTransform.rotation =
+                Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", createdTransform.rotation.eulerAngles));
+            createdTransform.localScale = EditorGUILayout.Vector3Field("Scale", createdTransform.localScale);
         }
+
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
         _building = GUILayout.Toggle(_building, "Start building", "Button", GUILayout.Height(60));
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
@@ -71,8 +76,8 @@ public class LevelBuilder : EditorWindow
         EditorGUILayout.EndScrollView();
         EditorGUILayout.EndVertical();
     }
-    
-    private void OnSceneGUI(SceneView sceneView)
+
+    /*private void OnSceneGUI(SceneView sceneView)
     {
         if (_building)
         {
@@ -88,6 +93,61 @@ public class LevelBuilder : EditorWindow
                 sceneView.Repaint();
             }
         }
+    }*/
+
+    private void OnSceneGUI(SceneView sceneView)
+    {
+        if (_building)
+        {
+            if (_createdObject == null)
+                CreateObject();
+
+            if (Raycast(out Vector3 contactPoint))
+            {
+                DrawPointer(contactPoint, Color.red);
+                _createdObject.transform.position = contactPoint;
+
+                if (CheckRotationInput(out Vector3 rotation))
+                {
+                    Quaternion quaternion = _createdObject.transform.rotation;
+                    quaternion.eulerAngles = rotation;
+                    _createdObject.transform.rotation = quaternion;
+                }
+
+                if (CheckPlacementInput())
+                {
+                    _building = false;
+                    _createdObject = null;
+                }
+
+                sceneView.Repaint();
+            }
+        }
+    }
+
+    private bool CheckRotationInput(out Vector3 rotation)
+    {
+        rotation = _createdObject.transform.rotation.eulerAngles;
+
+        Debug.Log(Event.current.type);
+        if (Event.current.type == EventType.KeyDown)
+        {
+            Debug.Log(Event.current.keyCode);
+            
+            if (Event.current.keyCode == KeyCode.Q)
+            {
+                rotation.y-=_rotationSpeed;
+                return true;
+            }
+            
+            if (Event.current.keyCode == KeyCode.E)
+            {
+                rotation.y+=_rotationSpeed;
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     private bool Raycast(out Vector3 contactPoint)
@@ -95,7 +155,8 @@ public class LevelBuilder : EditorWindow
         Ray guiRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         contactPoint = Vector3.zero;
 
-        if (Physics.Raycast(guiRay, out RaycastHit raycastHit))
+        if (Physics.Raycast(guiRay, out RaycastHit raycastHit, Single.PositiveInfinity,
+                LayerMask.GetMask(LayerMask.LayerToName(_parent.layer))))
         {
             contactPoint = raycastHit.point;
             return true;
@@ -104,20 +165,20 @@ public class LevelBuilder : EditorWindow
         return false;
     }
 
-    private void DrawPounter(Vector3 position, Color color)
+    private void DrawPointer(Vector3 position, Color color)
     {
         Handles.color = color;
         Handles.DrawWireCube(position, Vector3.one);
     }
 
-    private bool CheckInput()
+    private bool CheckPlacementInput()
     {
         HandleUtility.AddDefaultControl(0);
 
         return Event.current.type == EventType.MouseDown && Event.current.button == 0;
     }
 
-    private void CreateObject(Vector3 position)
+    /*private void CreateObject(Vector3 position)
     {
         if (_selectedElement < _catalog.Count)
         {
@@ -128,11 +189,27 @@ public class LevelBuilder : EditorWindow
 
             Undo.RegisterCreatedObjectUndo(_createdObject, "Create");
         }
+    }*/
+
+    private void CreateObject()
+    {
+        if (_selectedElement < _catalog.Count == false)
+            return;
+
+        GameObject prefab = _catalog[_selectedElement];
+        _createdObject = Instantiate(prefab, _parent.transform, true);
+        Undo.RegisterCreatedObjectUndo(_createdObject, "Create");
     }
+
+    /*private void DrawCatalog(List<GUIContent> catalogIcons, int width, int height)
+    {
+        _selectedElement = GUILayout.SelectionGrid(_selectedElement, catalogIcons.ToArray(), 4, GUILayout.Width(width), GUILayout.Height(height));
+    }*/
 
     private void DrawCatalog(List<GUIContent> catalogIcons, int width, int height)
     {
-        _selectedElement = GUILayout.SelectionGrid(_selectedElement, catalogIcons.ToArray(), 4, GUILayout.Width(width), GUILayout.Height(height));
+        _selectedElement = GUILayout.SelectionGrid(_selectedElement, catalogIcons.ToArray(), 4, GUILayout.Width(width),
+            GUILayout.Height(height));
     }
 
     private List<GUIContent> GetCatalogIcons()
